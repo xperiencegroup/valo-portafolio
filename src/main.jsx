f265bf827f7a7827a4a01e78b0135cc502154899
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-03 13:34:22 -0600

    Definiciójn y uso de fuente principal "Eurostile Next"

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "@/styles/fonts.css";
 import Router from "./router/router.jsx";
 import { AppContextProvider } from "./context/AppProvider.jsx";
 
