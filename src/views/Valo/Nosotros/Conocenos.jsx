5a293576bd5c118b1c8e120c29e2a39862c91c25
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-04 15:23:29 -0600

    Cambios mínimos y videos ejemplo

--- src/views/Valo/Nosotros/Conocenos.jsx
+++ src/views/Valo/Nosotros/Conocenos.jsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function Conocenos() {
   return (
     <div className="w-dvw h-dvh p-5 flex flex-col gap-6">
-      <h1 className="text-valo">VALO</h1>
+      <h1 className="text-valo text-5xl">VALO</h1>
       <div className="flex flex-1 bg-white">
         <ConoceValoCarousel />
       </div>
