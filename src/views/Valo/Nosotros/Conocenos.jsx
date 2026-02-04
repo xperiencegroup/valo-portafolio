5dbbed9023a1de46e9d644710e5a41e801a58a70
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-04 01:56:01 -0600

    Componente de ruta "/nosotros/conoce-valo-developers"

---
+++ src/views/Valo/Nosotros/Conocenos.jsx
@@ -0,0 +1,17 @@
+import { ConoceValoCarousel } from "@/components/Carousel/ConoceValoCarousel";
+import BackButton from "@/components/Navigation/BackButton";
+import React from "react";
+
+export default function Conocenos() {
+  return (
+    <div className="w-dvw h-dvh p-5 flex flex-col gap-6">
+      <h1 className="text-valo">VALO</h1>
+      <div className="flex flex-1 bg-white">
+        <ConoceValoCarousel />
+      </div>
+      <div className="relative">
+        <BackButton />
+      </div>
+    </div>
+  );
+}
