dbafd3e581122509c7e356f010cfffe77a4871db
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-04 01:54:44 -0600

    Layout vacío para manejar rutas anidadas sin componentes padres que se rendericen, ruta "conoce-valo-developers" creada

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,13 +3,18 @@ import Home from "@/views/Valo/Home/Home";
 import Proyectos from "@/views/Valo/Proyectos/Proyectos";
 import Contacto from "@/views/Valo/Contacto/Contacto";
 import Nosotros from "@/views/Valo/Nosotros/Nosotros";
+import Conocenos from "@/views/Valo/Nosotros/Conocenos";
+import NosotrosLayout from "@/views/Valo/Nosotros/NosotrosLayout";
 
 export default function Router() {
   return (
     <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/nosotros" element={<Nosotros />} />
+        <Route path="/nosotros" element={<NosotrosLayout />}>
+          <Route index element={<Nosotros />} />
+          <Route path="conoce-valo-developers" element={<Conocenos />} />
+        </Route>
         <Route path="/proyectos" element={<Proyectos />} />
         <Route path="/contacto" element={<Contacto />} />
       </Routes>
