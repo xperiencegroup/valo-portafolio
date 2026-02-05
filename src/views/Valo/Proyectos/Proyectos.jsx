ead48d0ea1825e23e26d26e007c126b57d42c318
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-05 02:16:09 -0600

    UI terminada de sección proyectos utilizando mapa interactivo SVG

--- src/views/Valo/Proyectos/Proyectos.jsx
+++ src/views/Valo/Proyectos/Proyectos.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import masterplanBg from "@/assets/images/proyectos/masterplan.jpg";
+import { MapaMexico } from "@/assets/images/proyectos/MapaMexico";
+
+const ACTIVE_STATES = ["nuevo-leon", "guanajuato"];
 
 export default function Proyectos() {
-  return <div>Proyectos</div>;
+  return (
+    <div className="h-dvh w-dvw">
+      {/* Background image */}
+      <img
+        src={masterplanBg}
+        alt="Imagen del masterplan de fondo"
+        className="absolute z-0 inset-0 w-full h-full object-cover"
+      />
+
+      {/* Overlay */}
+      <div className="absolute z-0 inset-0 w-full h-full bg-[#12274C]/85 backdrop-blur-md" />
+
+      {/* Content */}
+      <div className="relative w-full h-full flex justify-center items-center">
+        <MapaMexico ACTIVE_STATES={ACTIVE_STATES} />
+      </div>
+    </div>
+  );
 }
