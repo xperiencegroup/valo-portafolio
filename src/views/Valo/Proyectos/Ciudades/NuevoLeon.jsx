b7a93e73a296d8d28432f6f6da489be67b44660c
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-09 14:52:55 -0600

    UI de ruta proyectos/nuevo-leon terminado (sin utilizar mapa 3D interactivo)

--- src/views/Valo/Proyectos/Ciudades/NuevoLeon.jsx
+++ src/views/Valo/Proyectos/Ciudades/NuevoLeon.jsx
@@ -1,9 +1,19 @@
+import MapaMonterrey from "@/assets/images/proyectos/MapaMonterrey";
+import { MapFilterButtons } from "@/components/Buttons/MapFilterButtons";
 import BackButton from "@/components/Navigation/BackButton";
 
 export default function NuevoLeon() {
   return (
-    <div className="w-dvw h-dvh bg-blue-800">
-      NuevoLeon {/* Return button */}
+    <div className="w-dvw h-dvh">
+      {/* Filter Buttons */}
+      <MapFilterButtons />
+
+      {/* Map and Markers */}
+      <div className="absolute z-0 inset-0">
+        <MapaMonterrey />
+      </div>
+
+      {/* Return button */}
       <div className="fixed bottom-0 p-5 z-30">
         <BackButton to="/proyectos" />
       </div>
