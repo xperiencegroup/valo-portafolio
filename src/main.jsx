b784d9a185a9102a6fd1f941e1919950730af7eb
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-12 13:06:22 -0600

    CSS default para mapas de mapbbox

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "@/styles/fonts.css";
+import "mapbox-gl/dist/mapbox-gl.css";
 import Router from "./router/router.jsx";
 import { AppContextProvider } from "./context/AppProvider.jsx";
 
