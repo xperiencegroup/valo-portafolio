76b504e1a5e52dbe148c969a0309e9f3da3574a1
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-13 20:15:06 -0600

    Responsive para submenu "Nosotros"

--- src/components/Menus/NosotrosSubMenu.jsx
+++ src/components/Menus/NosotrosSubMenu.jsx
@@ -7,16 +7,19 @@ export default function NosotrosSubMenu() {
   const nosotrosSubMenu = menu[0];
 
   return (
-    <div className="px-5 py-6.25 bg-valo">
-      <p className="flex justify-center items-center w-67.75 h-17 px-6.25 py-3.75 text-[32px] uppercase">
+    <div className="w-[clamp(180px,16.2vw,311px)] py-2 px-3 2xl:px-5 xl:py-6.25 bg-valo">
+      <p className="flex justify-center items-center w-full max-w-67.75 2xl:h-17 py-1 2xl:px-6.25 2xl:py-3.75 text-title-button uppercase">
         Menú
       </p>
 
-      <span className="flex items-center justify-between w-full h-17.5 px-6.25 rounded-[40px] bg-white">
-        <p className="text-[21px] rounded-[40px] font-bold text-valo">
+      <span className="flex items-center justify-between w-full h-[clamp(40px,3.65vw,70px)] px-3.5 2xl:px-6.25 rounded-[40px] bg-white">
+        <p className="text-paragraph-button rounded-[40px] font-bold text-valo">
           Nosotros
         </p>
-        <CircleIndicatorIcon isActive className="size-6" />
+        <CircleIndicatorIcon
+          isActive
+          className="size-[clamp(16px,1.25vw,24px)]"
+        />
       </span>
 
       <nav className="flex flex-col">
@@ -25,11 +28,11 @@ export default function NosotrosSubMenu() {
             <Link
               key={section.id}
               to={section.to}
-              className="group flex items-center justify-between w-full h-11.25 px-6.25 text-[17px] font-semibold tracking-tighter rounded-[40px] hover:bg-white hover:text-valo hover:font-bold hover:cursor-pointer"
+              className="group flex items-center justify-between w-full h-[clamp(32px,2.34vw,45px)] px-3.5 2xl:px-6.25 text-paragraph-sub-button font-semibold tracking-tighter rounded-[40px] hover:bg-white hover:text-valo hover:font-bold hover:cursor-pointer"
             >
               {section.label}
 
-              <span className="relative w-6 h-6">
+              <span className="relative size-[clamp(16px,1.25vw,24px)]">
                 {/* Inactive */}
                 <span className="absolute inset-0 group-hover:hidden">
                   <CircleIndicatorIcon isActive={false} />
--- src/views/Valo/Nosotros/NosotrosLayout.jsx
+++ src/views/Valo/Nosotros/NosotrosLayout.jsx
@@ -4,8 +4,8 @@ import LogoValoMark from "@/assets/logos/logo-valo-mark";
 export default function NosotrosLayout() {
   return (
     <>
-      <div className="absolute top-0 left-0 pt-13 pl-15 z-50">
-        <LogoValoMark className="text-valo" />
+      <div className="absolute top-0 left-0 p-8 lg:pt-13 lg:pl-15 z-50">
+        <LogoValoMark className="w-[clamp(90px,7.34vw,141px)] text-valo" />
       </div>
 
       <Outlet />
