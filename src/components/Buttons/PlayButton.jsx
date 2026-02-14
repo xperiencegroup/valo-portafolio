d0a997d0d4b59e8307cac1d1a55b9d41ecf29993
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-13 18:49:30 -0600

    Botón responsive con icono aceptando clases de tailwind

--- src/components/Buttons/PlayButton.jsx
+++ src/components/Buttons/PlayButton.jsx
@@ -2,14 +2,16 @@ import { VideoIcon } from "@/assets/icons/VideoIcon";
 
 export default function PlayButton() {
   return (
-    <div className="group flex justify-center items-center size-22.5 bg-valo hover:bg-white hover:cursor-pointer">
-      {/* Inactive */}
-      <span className="group-hover:hidden">
-        <VideoIcon isActive={false} />
-      </span>
-      {/* Hover / Active */}
-      <span className="hidden group-hover:block">
-        <VideoIcon isActive />
+    <div className="group flex justify-center items-center size-[clamp(65px,4.69vw,90px)] bg-valo hover:bg-white hover:cursor-pointer">
+      <span className="relative size-[clamp(38px,2.6vw,50px)]">
+        {/* Inactive */}
+        <span className="absolute inset-0 group-hover:hidden">
+          <VideoIcon isActive={false} />
+        </span>
+        {/* Hover / Active */}
+        <span className="absolute inset-0 hidden group-hover:block">
+          <VideoIcon isActive />
+        </span>
       </span>
     </div>
   );
